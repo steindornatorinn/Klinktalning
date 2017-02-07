bdac1b361c9bb7a5c96c7d78177c19b96fa228e0
Steindor Bragason <steinibragason@gmail.com>
2017-02-07 22:19:43 +0000

    solved the unicode issues

--- klinktalning.py
+++ klinktalning.py
@@ -1,27 +1,40 @@
 #!/usr/bin/env python
-# -*- encoding: UTF-8 -*-
+# -*- coding: utf-8 -*-
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+try:
+    os.system("chcp 65001")
+    os.system("cls")
+except:
+    raise
 WEIGHT_100 = 8.5
 WEIGHT_50 = 8.25
 WEIGHT_10 = 6.9
 WEIGHT_5 = 6.5
 WEIGHT_1 = 4.5
 
-WELCOME_MESSAGE = u"Athugið að þetta forrit tekur allar upplýsingar inn sem tölur í grömmum. t.d skal tákna 4 kíló og 350 grömm sem 4350"
-IN_MESSAGE_100 = u"Stimplið inn þyngd Hundraðkallanna\n"
-IN_MESSAGE_50 = u"Stimplið inn þyngd fimmtíukallanna\n"
-IN_MESSAGE_10 = u"Stimplið inn þyngd tíkallanna\n"
-IN_MESSAGE_5 = u"Stimplið inn þyngd fimmkallanna\n"
-IN_MESSAGE_1 = u"Stimplið inn þyngd einakrónanna\n"
-IN_MESSAGE_CONTAINER = u"Stimplið inn þyngd ílatsins sem klinkið var vigað í, sláið inn 0 ef þetta á ekki við\n"
 
-OUT_MESSAGE_100 = u"Heildarvirði hundraðkallanna: "
-OUT_MESSAGE_50 = u"Heildarvirði fimmtíukallanna: "
-OUT_MESSAGE_10 = u"Heildarvirði tíkallanna: "
-OUT_MESSAGE_5 = u"Heildarvirði fimmkallanna: "
-OUT_MESSAGE_1 = u"Heildarvirði einakrónanna: "
-OUT_MESSAGE_TOTAL = u"Heildarvirði: "
+WELCOME_MESSAGE = u"Athugið að þetta forrit tekur allar upplýsingar inn sem eina tölu sem táknar grammafjölda. t.d skal stimpla inn 4 kíló og 350 grömm sem töluna 4350".encode('utf-8')
+IN_MESSAGE_100 = u"Stimplið inn þyngd Hundraðkallanna\n".encode('utf-8')
+IN_MESSAGE_50 = u"Stimplið inn þyngd fimmtíukallanna\n".encode('utf-8')
+IN_MESSAGE_10 = u"Stimplið inn þyngd tíkallanna\n".encode('utf-8')
+IN_MESSAGE_5 = u"Stimplið inn þyngd fimmkallanna\n".encode('utf-8')
+IN_MESSAGE_1 = u"Stimplið inn þyngd einakrónanna\n".encode('utf-8')
+IN_MESSAGE_CONTAINER = u"Stimplið inn þyngd ílátsins sem klinkið var vigað í, sláið annars inn 0 ef þetta á ekki við\n".encode('utf-8')
+LINE = u"------------------------------------------------------\n".encode('utf-8')
+
+OUT_MESSAGE_100 = u"Heildarvirði hundraðkallanna: ".encode('utf-8')
+OUT_MESSAGE_50 = u"Heildarvirði fimmtíukallanna: ".encode('utf-8')
+OUT_MESSAGE_10 = u"Heildarvirði tíkallanna: ".encode('utf-8')
+OUT_MESSAGE_5 = u"Heildarvirði fimmkallanna: ".encode('utf-8')
+OUT_MESSAGE_1 = u"Heildarvirði einakrónanna: ".encode('utf-8')
+OUT_MESSAGE_TOTAL = u"Heildarvirði: ".encode('utf-8')
+
 
 print(WELCOME_MESSAGE)
+print(LINE)
 container_weight = input(IN_MESSAGE_CONTAINER)
 total_weight_100 = input(IN_MESSAGE_100)
 total_weight_50 = input(IN_MESSAGE_50)
@@ -40,4 +53,4 @@ print(OUT_MESSAGE_10 + str(value_10))
 print(OUT_MESSAGE_5 + str(value_5))
 print(OUT_MESSAGE_1 + str(value_1))
 print(OUT_MESSAGE_TOTAL + str(value_total))
-
+raw_input()
